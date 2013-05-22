62e3f7ebf2ac95b15b423683da152ccfa861137d
Niket Desai <iamniket@gmail.com>
2013-05-21 19:03:08 -0700

    removed notify & notify routes to prevent location subscription and pushes

--- main.py
+++ main.py
@@ -25,13 +25,12 @@ import webapp2
 
 from attachmentproxy.handler import ATTACHMENT_PROXY_ROUTES
 from main_handler import MAIN_ROUTES
-from notify.handler import NOTIFY_ROUTES
 from oauth.handler import OAUTH_ROUTES
 from signout.handler import SIGNOUT_ROUTES
 
 
 ROUTES = (
-    ATTACHMENT_PROXY_ROUTES + MAIN_ROUTES + NOTIFY_ROUTES + OAUTH_ROUTES +
+    ATTACHMENT_PROXY_ROUTES + MAIN_ROUTES + OAUTH_ROUTES +
     SIGNOUT_ROUTES)
 
 
